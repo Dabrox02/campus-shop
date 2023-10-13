f830104f3256e37c955512f169be9107ec4bba50
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-13 09:32:46 -0500

    feat: :sparkles: load all categories
    {

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 
+import { loadCoats, loadShirts, loadPants } from "./modules/loadProducts.js"
+
 const d = document;
 const $ = (e) => d.querySelector(e);
 const $a = (e) => d.querySelectorAll(e);
 
 export const app = async () => {
 
+    let path = window.location.pathname.split(".")[0];
+
+    if (path === "/index" || path === "/") {
+    }
+
+    if (path === "/views/todos") {
+        await loadCoats($("#grid-products-cards"));
+        await loadPants($("#grid-products-cards"));
+        await loadShirts($("#grid-products-cards"));
+    }
+
+    if (path === "/views/abrigos") {
+        await loadCoats($("#grid-products-cards"));
+    }
+
+    if (path === "/views/camisetas") {
+        await loadShirts($("#grid-products-cards"));
+    }
+
+    if (path === "/views/pantalones") {
+        await loadPants($("#grid-products-cards"));
+    }
+
+    if (path === "/views/carrito") {
+    }
 }
---
+++ modules/loadProducts.js
@@ -0,0 +1,32 @@
+import coatModel from "../api/models/coatModel.js"
+import pantModel from "../api/models/pantModel.js"
+import shirtModel from "../api/models/shirtModel.js"
+
+export const loadCoats = async (grid) => {
+    let coats = await coatModel.getAll();
+    coats.forEach(c => {
+        grid.insertAdjacentHTML("beforeend", /*html*/`
+        <product-component img="${c.imagen}" price="${c.precio}" nameProduct="${c.nombre}" idBtn="${c.id}" category="abrigo"></product-component>
+        `)
+    });
+}
+
+export const loadShirts = async (grid) => {
+    let shirts = await shirtModel.getAll();
+    shirts.forEach(c => {
+        grid.insertAdjacentHTML("beforeend", /*html*/`
+        <product-component img="${c.imagen}" price="${c.precio}" nameProduct="${c.nombre}" idBtn="${c.id}" category="camiseta"></product-component>
+        `)
+    });
+}
+
+
+export const loadPants = async (grid) => {
+    let pants = await pantModel.getAll();
+    pants.forEach(c => {
+        console.log(c);
+        grid.insertAdjacentHTML("beforeend", /*html*/`
+        <product-component img="${c.imagen}" price="${c.precio}" nameProduct="${c.nombre}" idBtn="${c.id}" category="abrigo"></product-component>
+        `)
+    });
+}
